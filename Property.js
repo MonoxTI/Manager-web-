9d66bb3615b08a00e0b67497cce4e55653edfbe0
MonoxTI <itumonokoane84@gmail.com>
2025-06-10 22:50:55 +0200

    set up

---
+++ Property.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const host = 'localhost';
+const port = 8000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.URL);
+
